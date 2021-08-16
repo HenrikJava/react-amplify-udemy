7c44038488a5aa382bc2badb8a2c386c5edd65d4
HenrikJava <henriklarssonmail@gmail.com>
2021-08-16 11:26:42 +0200

    changed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import Amplify from 'aws-amplify'
+import awsConfig from './aws-exports'
+import {AmplifyAuthenticator, AmplifySignOut} from '@aws-amplify/ui-react'
+Amplify.configure(awsConfig)
 
 function App() {
   return (
+    <AmplifyAuthenticator>
     <div className="App">
      <h1>Welcome to Amplify</h1>
     </div>
+    <AmplifySignOut></AmplifySignOut>
+    </AmplifyAuthenticator>
   );
 }
 
